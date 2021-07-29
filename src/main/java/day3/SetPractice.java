a72ad28f9701e082bd8d2771b93e07ad096616b2
Larisa Dzirkal <larisa36g@gmail.com>
2021-07-29 14:46:05 -0400

    added another info

--- src/main/java/day3/SetPractice.java
+++ src/main/java/day3/SetPractice.java
@@ -3,5 +3,7 @@ package day3;
 public class SetPractice {
     public static void main(String[] args) {
  // set is a collection type that only store unique items
+        // if you add duplicate it will just remove it
+
     }
 }
