e504c2cf37314f84da0a7b179b2e3fc9b9a01f2f
Larisa Dzirkal <larisa36g@gmail.com>
2021-07-29 14:48:19 -0400

    finished commits

--- src/main/java/day3/SetPractice.java
+++ src/main/java/day3/SetPractice.java
@@ -4,6 +4,7 @@ public class SetPractice {
     public static void main(String[] args) {
  // set is a collection type that only store unique items
         // if you add duplicate it will just remove it
+        // it is  collection of unique elements
 
     }
 }
